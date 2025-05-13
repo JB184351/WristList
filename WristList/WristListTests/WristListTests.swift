e7ed7220a2ed660f3dfe3319b78d48eff8fd9def
JB184351 <justinbengtson@icloud.com>
2025-05-13 15:40:03 -0700

    Initial Project Creation

---
+++ WristList/WristListTests/WristListTests.swift
@@ -0,0 +1,17 @@
+//
+//  WristListTests.swift
+//  WristListTests
+//
+//  Created by Justin on 5/13/25.
+//
+
+import Testing
+@testable import WristList
+
+struct WristListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
